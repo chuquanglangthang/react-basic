3faad769facbd26c6748e919093ccab73e195ca1
Dang Quang <quangdang229@gmail.com>
2022-05-25 16:35:22 +0700

    create own component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class MyComponent extends React.Component {
+    render(){
+        let name = 'John'
+        return (
+            <p>Hello from {name}.</p>
+        )
+    }
+}
+
+export default MyComponent
