e9dfcf912e6720183a735fb25b7860f70d6dc0e2
Dang Quang <quangdang229@gmail.com>
2022-05-25 16:47:40 +0700

    shortcut for React.Fragment

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -4,10 +4,10 @@ class MyComponent extends React.Component {
     render(){
         let name = 'John'
         return (
-            <React.Fragment>
+            <>
                 <p>Hello from {name}</p>
                 <p>Hello from {name}</p>
-            </React.Fragment>
+            </>
         )
     }
 }
